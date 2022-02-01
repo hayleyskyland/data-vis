2fbd081d588ab703164cb227fef554318ee427d6
hayleyskyland <hwitherell@skylandanalytics.net>
2022-02-01 11:53:30 -0700

    Change var to const

--- js-client/index.js
+++ js-client/index.js
@@ -1,4 +1,4 @@
-var data = [
+const data = [
     {
         "year": "2010",
         "amount": 134
@@ -45,13 +45,13 @@ var data = [
     }
 ]
 
-var gfgDb = opendatabase('mydb', '1.0', 'this is a client side database', 2 * 1024 * 1024);
+const gfgDb = opendatabase('mydb', '1.0', 'this is a client side database', 2 * 1024 * 1024);
 
 if (!gfgDb) {
     alert('database not created');
 }
 else {
-    var version = gfgDb.version;
+    const version = gfgDb.version;
 }
 
 async function callApis() {
@@ -69,9 +69,9 @@ callApis()
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then(async (response) => {
         fetchedData = await response.json()
-        var svg = d3.select("#data-vis-container")
+        const svg = d3.select("#data-vis-container")
 
-        var margin = 200,
+        const margin = 200,
             width = svg.attr("width") - margin,
             height = svg.attr("height") - margin
 
@@ -82,10 +82,10 @@ fetch('https://jsonplaceholder.typicode.com/posts')
             .attr("font-size", "24px")
             .text("Example fluctuations")
 
-        var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+        const xScale = d3.scaleBand().range([0, width]).padding(0.4),
             yScale = d3.scaleLinear().range([height, 0]);
 
-        var g = svg.append("g")
+        const g = svg.append("g")
             .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
         xScale.domain(fetchedData.map(function (d) { return d.userId; }));
